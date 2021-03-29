019e3767d14f596924ad42b6680ffd1d7dc5e7cc
agneshew <agnes.hewusz@gmail.com>
2021-03-29 20:45:23 +0200

    tasks

--- src/main/java/warmUp1/WarmUp1Main.java
+++ src/main/java/warmUp1/WarmUp1Main.java
@@ -23,7 +23,28 @@ public class WarmUp1Main {
         System.out.println("Task 6 \nGiven 2 ints, a and b, return true if one if them is 10 or if their sum is 10.");
         System.out.println(warmUp1Main.makes10(7, 10));
 
+        System.out.println("Task 7\nGiven an int n, return true if it is within 10 of 100 or 200. Note: Math.abs(num) computes the absolute value of a number.");
+        System.out.println(warmUp1Main.nearHundred(78));
+
+        System.out.println("Task 8\nGiven 2 int values, return true if one is negative and one is positive. Except if the parameter \"negative\" is true, then return true only if both are negative.");
+        System.out.println(warmUp1Main.posNeg(3, -5, true));
+
+        System.out.println("Task 9\nGiven a string, return a new string where \"not \" has been added to the front. However, if the string already begins with \"not\", return the string unchanged. Note: use .equals() to compare 2 strings.");
+        System.out.println(warmUp1Main.notString("not enough"));
+
+        System.out.println("Task 10\nGiven a non-empty string and an int n, return a new string where the char at index n has been removed. The value of n will be a valid index of a char in the original string (i.e. n will be in the range 0..str.length()-1 inclusive).");
+        System.out.println(warmUp1Main.missingChar("developer", 7));
+
+        System.out.println("Task 11\nGiven a string, return a new string where the first and last chars have been exchanged.");
+        System.out.println(warmUp1Main.frontBack("java"));
+
+        System.out.println("Task 12\nGiven a string, we'll say that the front is the first 3 chars of the string. If the string length is less than 3, the front is whatever is there. Return a new string which is 3 copies of the front.");
+        System.out.println(warmUp1Main.front3("Junior"));
+
+        System.out.println("Task 13\nGiven a string, take the last char and return a new string with the last char added at the front and back, so \"cat\" yields \"tcatt\". The original string will be length 1 or more.");
+        System.out.println(warmUp1Main.backAround("candy"));
     }
+
     //Task1
     public boolean sleepIn ( boolean weekday, boolean vacation) {
         if (!weekday || vacation) {
@@ -78,10 +99,70 @@ public class WarmUp1Main {
         }
     }
     //Task7
-
-
-
-
+    public boolean nearHundred(int n) {
+        if ( (Math.abs(100 - n) <= 10) || (Math.abs(200 - n) <= 10 )) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+    //Task8
+    public boolean posNeg(int a, int b, boolean negative) {
+        if (( !negative && ( ( a > 0 && b < 0) || ( a < 0 && b > 0)))
+                || ( negative && a < 0 && b < 0)) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+    //Task9
+    public String notString(String str) {
+        if (str.startsWith("not")) {
+            return str;
+        }
+        else {
+            return "not "+str;
+        }
+    }
+    //Task10
+    public String missingChar(String str, int n) {
+        String front = str.substring(0, n);
+        String back = str.substring(n+1,  str.length());
+        return front + back;
+    }
+    //Task11
+    public String frontBack(String str) {
+        if (str.length() <=1) {
+            return str;
+        }
+        else {
+            int n = str.length() -1;
+            String middle = str.substring(1,n);
+            return str.charAt(n) + middle + str.charAt(0);
+        }
+    }
+    //Task12
+    public String front3(String str) {
+        String front;
+        if (str.length() < 3) {
+            front = str;
+        }
+        else {
+            front = str.substring(0, 3);
+        }
+        return front + front + front;
+    }
+    //Task13
+    public String backAround(String str) {
+        if (str.length() > 0) {
+            return str.charAt(str.length()-1) + str + str.charAt(str.length()-1);
+        }
+        else {
+            return str;
+        }
+    }
 
 }
 
